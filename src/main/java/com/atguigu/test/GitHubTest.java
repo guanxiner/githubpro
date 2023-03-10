02d72601a721b03a0e99a6af6816e5d7b671fff6
王宝民 <1343686909@qq.com>
2023-03-10 11:39:28 +0800

    version1.0

---
+++ src/main/java/com/atguigu/test/GitHubTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.test;
+
+/**
+ * ClassName: GitHubTest
+ * Package: com.atguigu.test
+ * Description:
+ *
+ * @Author 王宝民
+ * @Create 2023/3/10 11:37
+ * @Version 1.0
+ */
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println("push github test");
+    }
+}
